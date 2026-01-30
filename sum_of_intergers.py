47c949de95d6f008836bfc5deced067c5cc7e12b
Subrat Kumar Barik <subratmca85@gmail.com>
2026-01-30 12:01:20 +0530

    Update sum_of_intergers.py

--- sum_of_intergers.py
+++ sum_of_intergers.py
@@ -2,11 +2,12 @@
 This program returns the sum of integers from 1 to 50 using loop
 
 This can be done is two ways:
-1) Creating a list of 50 items, starting from 1 to 50 ex: nums = [1,2,3...,49,50]
-2) By using range()
+1) Creating a list of 50 items, starting from 1 to 50 ex: nums = [1,2,3...,49,50] and iterate it to get the sum
+2) By using range(). I have used this for professional approach
 """
 sum = 0
 
 for i in range(1, 51):
     sum += i
+
 print(f"The sum of the numbers from 1 to 50 is: {sum}")
